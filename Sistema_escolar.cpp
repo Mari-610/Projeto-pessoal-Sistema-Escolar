53565ba17f50d0dded0626ec40409d76591658bb
Marina Vieira Prado <pradomari403@gmail.com>
2026-09-04 23:47:51 -0300

    Primeira parte, cadastro dos alunos

---
+++ Sistema_escolar.cpp
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+#include<stdlib.h>
+int notas(float media) {
+	float N[4];
+	printf("1 bimestre:\n");
+	scanf ("%f", &N[0]);
+	printf("2 bimestre:\n");
+	scanf("%f", &N[1]);
+	printf("3 bimestre:\n");
+	scanf("%f", &N[2]);
+	printf("4 bimestre:\n");
+	scanf("%f", &N[3]);
+	printf("As notas são:\n%f\n%f\n%f\n%f\n", N[0], N[1], N[2], N[3]);
+	media=((N[0]+N[1]+N[2]+N[3])/4);
+	printf("A média do aluno é: %.2f", media);
+	if (media>6) {
+		printf("Aprovado!\n");
+	}
+	else {
+		printf("Reprovado");
+	}
+}
+int curso (int op) {
+	if (op==1) {
+		printf("Aluno matriculado em Desenvolvimento de Sistemas");
+	}
+	else if (op==2){
+		printf("Aluno matriculado em Eletrônica");
+	}
+	else if (op==3){
+		printf("Aluno matriculado em Mecânica");
+	}
+	else if (op==4){
+		printf("Aluno matriculado em Sistemas de energia renovável");
+	}
+	else if (op==5){
+		printf("Aluno matriculado em Telecomunicações");
+	}
+	else {
+		printf("Inválido");
+	}
+}
+int main(void) {
+	setlocale(LC_ALL,"");
+	int menu;
+	int situacao;
+	int idade;
+	char nome[80];
+	char tel[20];
+	char cpf[16];
+	int op;
+	float media;
+	printf ("Olá, seja bem-vindo!");
+	printf("Insira abaixo o número correspondente a ação que deseja executar!");
+	printf("\n-------------------------------------------------------------------\n");
+	printf("\nInsira:\n (1) Cadastrar\n(2)Acessar\n(3)Alterar\n(4)Pesquisar\n(5)Remover\n(6)Sair");
+	scanf("%i", &menu);
+	switch (menu) {
+		case 1:
+		printf ("1 - CADASTRO\n");
+		printf ("Insira a situação da pessoa a ser cadastrada:\n(1)Aluno\n(2)Professor\n");
+		scanf("%i", &situacao);
+		if (situacao==1) {
+		printf ("BEM VINDO AO PORTAL DO ALUNO!");
+		printf ("Insira sua idade:\n");
+		scanf("%i", &idade);
+		while (getchar() != '\n');
+		printf("Insira seu nome completo:\n");
+		fgets (nome, 80, stdin);
+		printf ("Insira o número de telefone:\n");
+		fgets(tel, 20, stdin);
+		printf ("Insira seu CPF:\n");
+		fgets(cpf, 16, stdin);
+		printf("Insira seu curso:\n");
+		printf ("Escolha seu curso:(1)Desenvolvimento de Sistemas\n(2)Eletrônica\n(3)Mecânica\n(4)Energias renováveis\n(5)Telecomunicações\n");
+		scanf("%i", &op);
+		curso(op);
+		printf("\n");
+		printf("\nInsira as atuais notas dos alunos:\n");
+		notas(media);
+		break;
+		}
+		else {
+			printf ("BEM VINDO AO PORTAL DO PROFESSOR!");
+		}
+	}
+	return 0;
+}
