10f17f96c88ab0776ecb2e456b1fef7b364e7ab9
Marina Vieira Prado <pradomari403@gmail.com>
2026-09-06 00:06:02 -0300

    Conclusão do cadastro de alunos, definir cadastro de professores para consertar retorno

--- Sistema_escolar.cpp
+++ Sistema_escolar.cpp
@@ -2,7 +2,7 @@
 #include<locale.h>
 #include<string.h>
 #include<stdlib.h>
-int notas(float media) {
+float notas(float media) {
 	float N[4];
 	printf("1 bimestre:\n");
 	scanf ("%f", &N[0]);
@@ -15,12 +15,13 @@ int notas(float media) {
 	printf("As notas são:\n%f\n%f\n%f\n%f\n", N[0], N[1], N[2], N[3]);
 	media=((N[0]+N[1]+N[2]+N[3])/4);
 	printf("A média do aluno é: %.2f", media);
-	if (media>6) {
+	if (media>7) {
 		printf("Aprovado!\n");
 	}
 	else {
-		printf("Reprovado");
+		printf("Reprovado\n");
 	}
+	return media;
 }
 int curso (int op) {
 	if (op==1) {
@@ -42,6 +43,17 @@ int curso (int op) {
 		printf("Inválido");
 	}
 }
+
+struct cadastro {
+	int situacao;
+	int idade;
+	char nome[80];
+	char tel[20];
+	char cpf[16];
+	int op;
+	float media;
+	};
+	
 int main(void) {
 	setlocale(LC_ALL,"");
 	int menu;
@@ -52,39 +64,61 @@ int main(void) {
 	char cpf[16];
 	int op;
 	float media;
+	int pc;
+	int i;
 	printf ("Olá, seja bem-vindo!");
 	printf("Insira abaixo o número correspondente a ação que deseja executar!");
 	printf("\n-------------------------------------------------------------------\n");
-	printf("\nInsira:\n (1) Cadastrar\n(2)Acessar\n(3)Alterar\n(4)Pesquisar\n(5)Remover\n(6)Sair");
+	printf("\nInsira:\n (1) Cadastrar\n(2)Acessar\n(3)Alterar\n(4)Pesquisar\n(5)Remover\n(6)Sair\n");
 	scanf("%i", &menu);
 	switch (menu) {
 		case 1:
 		printf ("1 - CADASTRO\n");
+		printf ("Insira o número de pessoas que deseja cadastrar:\n");
+		scanf("%i", &pc);
+		struct cadastro pessoa[70];
+		for (i=0; i<pc; ++i) {
 		printf ("Insira a situação da pessoa a ser cadastrada:\n(1)Aluno\n(2)Professor\n");
-		scanf("%i", &situacao);
-		if (situacao==1) {
-		printf ("BEM VINDO AO PORTAL DO ALUNO!");
+		scanf("%i", &pessoa[i].situacao);
+		if (pessoa[i].situacao==1) {
+		printf ("\nBEM VINDO AO PORTAL DO ALUNO!\n");
 		printf ("Insira sua idade:\n");
-		scanf("%i", &idade);
+		scanf("%i", &pessoa[i].idade);
 		while (getchar() != '\n');
 		printf("Insira seu nome completo:\n");
-		fgets (nome, 80, stdin);
+		fgets (pessoa[i].nome, 80, stdin);
 		printf ("Insira o número de telefone:\n");
-		fgets(tel, 20, stdin);
+		fgets(pessoa[i].tel, 20, stdin);
 		printf ("Insira seu CPF:\n");
-		fgets(cpf, 16, stdin);
+		fgets(pessoa[i].cpf, 16, stdin);
 		printf("Insira seu curso:\n");
-		printf ("Escolha seu curso:(1)Desenvolvimento de Sistemas\n(2)Eletrônica\n(3)Mecânica\n(4)Energias renováveis\n(5)Telecomunicações\n");
-		scanf("%i", &op);
-		curso(op);
+		printf ("Escolha seu curso:\n(1)Desenvolvimento de Sistemas\n(2)Eletrônica\n(3)Mecânica\n(4)Energias renováveis\n(5)Telecomunicações\n");
+		scanf("%i", &pessoa[i].op);
+		curso(pessoa[i].op);
 		printf("\n");
 		printf("\nInsira as atuais notas dos alunos:\n");
-		notas(media);
-		break;
+		pessoa[i].media = notas(pessoa[i].media);
 		}
 		else {
-			printf ("BEM VINDO AO PORTAL DO PROFESSOR!");
+			printf ("\nBEM VINDO AO PORTAL DO PROFESSOR!\n");
 		}
+		}
+		system("cls");
+		printf ("\n__________PESSOAS CADASTRADAS__________\n");
+		for (i=0; i<pc; ++i) {
+			printf ("Pessoa: %s\n", pessoa[i].nome);
+			printf ("Idade: %i\n", pessoa[i].idade);
+			printf ("Telefone: %s\n", pessoa[i].tel);
+			printf("CPF: %s\n", pessoa[i].cpf);
+			if (pessoa[i].situacao==1) {
+				printf ("Média: %.2f\n", pessoa[i].media);
+			}
+			else {
+				printf ("Professor\n");
+			}
+		}
+		break;
+		}
+		return 0;
 	}
-	return 0;
-}
+
